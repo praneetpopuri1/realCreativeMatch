9a49c2ecf4f005527843186c932e70322eee5ca5
praneetpopuri1 <84326825+praneetpopuri1@users.noreply.github.com>
2022-03-10 08:21:14 -0600

    wish list...

---
+++ app/src/main/java/com/example/creativematch/FirebaseHelper.java
@@ -0,0 +1,195 @@
+package com.example.creativematch;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirebaseHelper {
+    private static String uid = null;            // var will be updated for currently signed in user
+    // inside MainActivity with the mAuth var
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    public FirebaseHelper() {
+        // will gel
+        //get refersence to or te of the auth and firestore elements
+        // these lines of code establish the conntetions to the auth and database we are linked to
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+    }
+
+    public FirebaseAuth getmAuth() {
+
+        return mAuth;
+    }
+
+    public void attachReadDataToUser() {
+        // need to put stuff here
+        if (getmAuth().getCurrentUser() != null){
+            uid =mAuth.getUid();
+            readData(new FirestoreCallback() {
+                @Override
+                public void onCallback(ArrayList<WishListItem> myList) {
+                    Log.i(TAG, "Inside attachReadDataToUser, onCallBack" + myList.toString());
+                }
+            });
+        }
+    }
+
+
+    public void addUserToFirestore(String name, String newUID) {
+    /*
+         This will add a document with the uid of the current user to the collection called "users"
+          For this we will create a Hash map since there are only two fields - a name and athe uid value
+        */
+        // the doc Id of the doc we are adding will be the same as the uid of the current user
+        // simmilar to making an object
+        Map<String, Object> user = new HashMap<>();
+
+        //put data into my obhject using a key value pair where i label each item I put in the map
+        // the key "name" is tje key that is used to label the data fireStore
+        // the parameter value of name is passed in to be the value assigned to name in firestore
+        user.put("name", name);
+
+        // this will create a new document in the collection "users" and assign it a docID that is = to newUID
+        db.collection("users").document(newUID)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.i(TAG, name + "'s user acount added");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "error adding user account");
+                    }
+                });
+
+
+
+
+
+    }
+
+    public void addData(WishListItem wish) {
+        //add a wishlist item to the data
+        // this method will be overloaded and the other method wull incorprate the to
+        // handle asynch calls for reading data to keep myItems al up to data
+        addData(wish, new FirestoreCallback() {
+            @Override
+            public void onCallback(ArrayList<WishListItem> myList) {
+
+            }
+        });
+    }
+
+
+
+    private void addData(WishListItem w, FirestoreCallback firestoreCallback)
+    {
+        db.collection("users").document(uid).collection("myWishList")
+                .add(w)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // we are going to update the document we JUST added by
+                        //editing the docID instance var so that it knows what
+                        // the calue is for its docID in firestore
+
+                        //in the onSuccess method, the document Reference parameter
+                        //contains a refernce to the newly create socument. We
+                        //can us this to extract the docID from firestore
+                        db.collection("users").document(uid).collection("myWishList")
+                                .document(documentReference.getId())
+                                .update("docID", documentReference.getId());
+                        Log.i(TAG, "just added " + w.getDocID());
+
+                        // if we want the arrarList to be updaetd now, we call
+                        // readData. If we dont care about comtuing our work, then
+                        // you dont need to call readData
+                        //later on practice expermient with commenting  this line out, see
+                        // gow it is different
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "error adding elemnt", e);
+                    }
+                });
+    }
+    public ArrayList<WishListItem> getWishListItems() {
+        return myItems;
+    }
+
+    public void editData(WishListItem w) {
+
+    }
+
+    public void deleteData(WishListItem w) {
+
+    }
+
+    public void updateUid(String uid) {
+
+    }
+
+    /* https://www.youtube.com/watch?v=0ofkvm97i0s
+    This video is good!!!   Basically he talks about what it means for tasks to be asychronous
+    and how you can create an interface and then using that interface pass an object of the interface
+    type from a callback method and access it after the callback method.  It also allows you to delay
+    certain things from occuring until after the onSuccess is finished.
+     */
+
+    private void readData(FirestoreCallback firestoreCallback) {
+        myItems.clear();  //clears out the array list so it gets a fresh copy of the data
+        db.collection("users").document(uid).collection("myWishList")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (DocumentSnapshot doc: task.getResult()){
+                                // processing of doc of the query we made
+                                // this is a snapshot of the sata at this moment in time when we
+                                // requested to get the data
+                                // to object allows us to pulll the document from the firestore objext
+                                // convert into a java object using the constutor
+                                WishListItem w = doc.toObject(WishListItem.class);
+                                myItems.add(w);
+
+                            }
+                            //still inside the onComplete method, I want to call my onCallback method
+                            // so tjat tje interface can do its job and let whoever is waiting know the
+                            // asych method is done
+
+                            Log.i(TAG, "sucess reading data" + myItems.toString());
+                            firestoreCallback.onCallback(myItems);
+                        }
+                    }
+                });
+    }
+
+    //https://stackoverflow.com/questions/48499310/how-to-return-a-documentsnapshot-as-a-result-of-a-method/48500679#48500679
+    public interface FirestoreCallback {
+        void onCallback (ArrayList<WishListItem> myList);
+    }
+}
--- app/src/main/java/com/example/creativematch/signUp.java
+++ app/src/main/java/com/example/creativematch/signUp.java
@@ -1,15 +1,70 @@
 package com.example.creativematch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class signUp extends AppCompatActivity {
+
+    public static FirebaseHelper firebaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        firebaseHelper = new FirebaseHelper();
 
+    }
+    public void signIn(View v) {
+        // Note we don't care what they entered for name here
+        // it could be blank
+
+        // Get user data
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        // verify all user data is entered
+        if (email.length() == 0 || password.length() == 0) {
+            Toast.makeText(getApplicationContext(), "Enter all fields", Toast.LENGTH_SHORT).show();
+        }
+
+        // verify password is at least 6 char long (otherwise firebase will deny)
+        else if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password must be at least 6 char long", Toast.LENGTH_SHORT).show();
+        }
+        else {
+
+            // code to sign in user
+            firebaseHelper.getmAuth().createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                firebaseHelper.updateUid(firebaseHelper.getmAuth().getCurrentUser().getUid());
+                                updateIfLoggedIn();
+                                Log.i(TAG, email + " logged in");
+
+                                firebaseHelper.attachReadDataToUser();
+
+                                Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                Log.d(TAG, "sigh In failed for " + email + " " + password);
+                            }
+                        }
+                    });
+
+
+        }
     }
 }

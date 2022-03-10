437c2fe1245eb61e1d229a358f16045c55fca488
praneetpopuri1 <84326825+praneetpopuri1@users.noreply.github.com>
2022-03-10 07:51:44 -0600

    sign up activity constraints places

--- app/src/main/java/com/example/creativematch/signUp.java
+++ app/src/main/java/com/example/creativematch/signUp.java
@@ -10,5 +10,6 @@ public class signUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
 }

02128c6956c4a6ab0734fb8d48b88eaff1799089
APURI12 <84340739+APURI12@users.noreply.github.com>
2022-05-12 08:16:53 -0500

    finished vid 6 grind this weekend

--- app/src/main/java/com/example/creativematch/activities/UsersActivity.java
+++ app/src/main/java/com/example/creativematch/activities/UsersActivity.java
@@ -3,14 +3,82 @@ package com.example.creativematch.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.example.creativematch.R;
+import com.example.creativematch.Utilities.Constants;
+import com.example.creativematch.Utilities.PreferenceManager;
+import com.example.creativematch.adapters.UsersAdapter;
+import com.example.creativematch.databinding.ActivityUsersBinding;
+import com.example.creativematch.models.User;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsersActivity extends AppCompatActivity {
+
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_users);
+        binding = ActivityUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
+        getUsers();
+    }
+
+    private void setListeners(){
+        binding.imageBack.setOnClickListener(v-> onBackPressed());
+    }
+
+    private void getUsers(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task->{
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.Key_USER_ID);
+                    if(task.isSuccessful() && task.getResult() != null){
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())){
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            users.add(user);
+                        }
+                        if(users.size()>0){
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.usersRecycleView.setAdapter(usersAdapter);
+                            binding.usersRecycleView.setVisibility(View.VISIBLE);
+                        } else{
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
+    }
+
+    private void showErrorMessage(){
+        binding.textErrorMessage.setText(String.format("%s", "No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void loading(Boolean isLoading){
+        if(isLoading){
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
     }
 }

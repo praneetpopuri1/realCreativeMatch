5b78e9e68c61acc75192e9fb47352e5a3c4a9733
praneetpopuri1 <84326825+praneetpopuri1@users.noreply.github.com>
2022-05-17 20:18:33 -0500

    made swipe left or right activity

---
+++ app/src/main/java/com/example/creativematch/activities/SwipeLeftOrRightOne.java
@@ -0,0 +1,14 @@
+package com.example.creativematch.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.creativematch.R;
+
+public class SwipeLeftOrRightOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_left_or_right_one);
+    }
+}

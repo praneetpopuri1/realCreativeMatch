2bd52b7a7419e9eb46a4bac6a01703cb682a73e1
praneetpopuri1 <84326825+praneetpopuri1@users.noreply.github.com>
2022-05-18 19:34:08 -0500

    token probably

--- app/src/main/java/com/example/creativematch/firebase/MessagingService.java
+++ app/src/main/java/com/example/creativematch/firebase/MessagingService.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -13,25 +15,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MessagingService extends FirebaseMessagingService {
+    public static FirebaseHelper firebaseHelper;
+    private FirebaseFirestore db;
+    public final String TAG = "Denna";
+
     @Override
     public void onNewToken(String token) {
         Map<String, Object> fName = new HashMap<>();
         fName.put("token", token);
-
+        FirebaseUser user = firebaseHelper.getmAuth().getCurrentUser();
 
         // this will create a new document in the collection "users" and assign it a docID that is = to newUID
-        db.collection("users").document(newUID)
+        db.collection("users").document(user.getUid())
                 .set(fName)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
-                        Log.i(TAG, name + "'s user acount added");
+                        Log.i(TAG,  "FCM token was updated");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "error adding user account");
+                        Log.d(TAG, "FCM token was not updated");
                     }
                 });
     }
